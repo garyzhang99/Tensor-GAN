362f1c118d557716be708143c8a9562723928e25
姜春阳 <17373488@buaa.edu.cn>
2020-01-31 17:32:12 +0800

    add my_gan

---
+++ my_gan.py
@@ -0,0 +1,142 @@
+#https://github.com/wiseodd/generative-models/blob/master/GAN/vanilla_gan/gan_tensorflow.py
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
+
+
+import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
+import numpy as np
+import matplotlib.pyplot as plt
+import my_util
+import matplotlib.gridspec as gridspec
+
+from util import get_number_parameters
+
+data_path = "./label1_data/"
+
+output_path = "./eye_out_label1/"
+
+file_name = os.listdir(data_path)
+
+filelist = [os.path.join(data_path, file) for file in file_name]
+
+data_size = len(filelist)
+
+whole_data = [tf.image.decode_png(tf.read_file(file, 'r')) for file in filelist]
+
+width = 640
+
+height = 480
+
+tunnel = 3
+
+pixel = width*height*tunnel
+
+batch_size = 8
+
+z_dim = 768
+
+h_dim = 5760
+
+def xavier_init(size):
+    in_dim = size[0]
+    xavier_stddev = 1. / tf.sqrt(in_dim / 2.)
+    return tf.random_normal(shape=size, stddev=xavier_stddev)
+
+
+X = tf.placeholder(tf.float32, shape=[None, pixel])
+
+D_W1 = tf.Variable(xavier_init([pixel, h_dim]))
+D_b1 = tf.Variable(tf.zeros(shape=[h_dim]))
+
+D_W2 = tf.Variable(xavier_init([h_dim, 1]))
+D_b2 = tf.Variable(tf.zeros(shape=[1]))
+
+theta_D = [D_W1, D_W2, D_b1, D_b2]
+
+
+Z = tf.placeholder(tf.float32, shape=[None, z_dim])
+
+G_W1 = tf.Variable(xavier_init([z_dim, h_dim]))
+G_b1 = tf.Variable(tf.zeros(shape=[h_dim]))
+
+G_W2 = tf.Variable(xavier_init([h_dim, pixel]))
+G_b2 = tf.Variable(tf.zeros(shape=[pixel]))
+
+theta_G = [G_W1, G_W2, G_b1, G_b2]
+
+print("Total number of parameters: {}".format(get_number_parameters(theta_G+theta_D)))
+
+def sample_Z(m, n):
+    return np.random.uniform(-1., 1., size=[m, n])
+
+
+def generator(z):
+    G_h1 = tf.nn.relu(tf.matmul(z, G_W1) + G_b1)
+    G_log_prob = tf.matmul(G_h1, G_W2) + G_b2
+    G_prob = tf.nn.sigmoid(G_log_prob)
+
+    return G_prob
+
+
+def discriminator(x):
+    D_h1 = tf.nn.relu(tf.matmul(x, D_W1) + D_b1)
+    D_logit = tf.matmul(D_h1, D_W2) + D_b2
+    D_prob = tf.nn.sigmoid(D_logit)
+
+    return D_prob, D_logit
+
+
+def plot(sample):
+    fig = plt.figure(figsize=(6.4, 4.8), dpi=100)
+    plt.imshow(sample.reshape(480, 640, 3))
+    plt.xticks([])
+    plt.yticks([])
+    plt.axis('off')
+    return fig
+
+
+G_sample = generator(Z)
+D_real, D_logit_real = discriminator(X)
+D_fake, D_logit_fake = discriminator(G_sample)
+
+# D_loss = -tf.reduce_mean(tf.log(D_real) + tf.log(1. - D_fake))
+# G_loss = -tf.reduce_mean(tf.log(D_fake))
+
+# Alternative losses:
+# -------------------
+D_loss_real = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=D_logit_real, labels=tf.ones_like(D_logit_real)))
+D_loss_fake = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=D_logit_fake, labels=tf.zeros_like(D_logit_fake)))
+D_loss = D_loss_real + D_loss_fake
+G_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=D_logit_fake, labels=tf.ones_like(D_logit_fake)))
+
+D_solver = tf.train.AdamOptimizer().minimize(D_loss, var_list=theta_D)
+G_solver = tf.train.AdamOptimizer().minimize(G_loss, var_list=theta_G)
+
+sess = tf.Session()
+sess.run(tf.global_variables_initializer())
+
+if not os.path.exists('out/'):
+    os.makedirs('out/')
+
+i = 0
+
+for it in range(1000000):
+    if it % 1000 == 0:
+        samples = sess.run(G_sample, feed_dict={Z: sample_Z(1, z_dim)})
+        fig = plot(samples[0])
+        plt.savefig(output_path + '{}.png'.format(str(i)), dpi=100)
+        i += 1
+        plt.close(fig)
+
+    X_mb = my_util.next_batch_line(whole_data=whole_data, data_size=data_size, batch_size=batch_size)
+    X_mb = [tensor.eval(session=sess) for tensor in X_mb]
+
+    _, D_loss_curr = sess.run([D_solver, D_loss], feed_dict={X: X_mb, Z: sample_Z(batch_size, z_dim)})
+    _, G_loss_curr = sess.run([G_solver, G_loss], feed_dict={Z: sample_Z(batch_size, z_dim)})
+
+    if it % 1000 == 0:
+        print('Iter: {}'.format(it))
+        print('D loss: {:.4}'. format(D_loss_curr))
+        print('G_loss: {:.4}'.format(G_loss_curr))
+        print()
